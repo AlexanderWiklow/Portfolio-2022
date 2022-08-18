2138eab2e5273baf028a98bb2b22b73681f1978c
AlexanderWiklow <a.wiklow@gmail.com>
2022-08-18 18:29:51 +0200

    Remove default-projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -19,48 +19,48 @@ import IMG15 from '../../assets/portfolio15.jpg'
 // DO NOT USE THE IMAGES IN PRODUCTION
 
 const data = [
-  {
-    id: 1,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
-  },
-  {
-    id: 2,
-    image: IMG2,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-  },
-  {
-    id: 3,
-    image: IMG3,
-    title: 'Figma dashboard UI kit for data design web apps',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: 'Maintaining tasks and tracking progress',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
-  },
+  // {
+  //   id: 1,
+  //   image: IMG1,
+  //   title: 'Crypto Currency Dashboard & Financial Visualization',
+  //   github: 'https://github.com',
+  //   demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+  // },
+  // {
+  //   id: 2,
+  //   image: IMG2,
+  //   title: 'Charts templates & infographics in Figma',
+  //   github: 'https://github.com',
+  //   demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  // },
+  // {
+  //   id: 3,
+  //   image: IMG3,
+  //   title: 'Figma dashboard UI kit for data design web apps',
+  //   github: 'https://github.com',
+  //   demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+  // },
+  // {
+  //   id: 4,
+  //   image: IMG4,
+  //   title: 'Maintaining tasks and tracking progress',
+  //   github: 'https://github.com',
+  //   demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+  // },
+  // {
+  //   id: 5,
+  //   image: IMG5,
+  //   title: 'Charts templates & infographics in Figma',
+  //   github: 'https://github.com',
+  //   demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  // },
+  // {
+  //   id: 6,
+  //   image: IMG6,
+  //   title: 'Charts templates & infographics in Figma',
+  //   github: 'https://github.com',
+  //   demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  // },
     {
     id: 7,
     image: IMG7,
